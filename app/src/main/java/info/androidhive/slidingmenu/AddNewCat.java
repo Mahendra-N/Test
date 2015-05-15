3e2ac0458748c65d0a2a9d8e29346c5ea6665d5c
Mahendra N <mahendra.n@ellipsonic.com>
2015-05-15 11:45:47 +0530

    UI in native done

---
+++ app/src/main/java/info/androidhive/slidingmenu/AddNewCat.java
@@ -0,0 +1,51 @@
+package info.androidhive.slidingmenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+
+public class AddNewCat extends Activity {
+    public ImageView backButton_cat = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_cat);
+        getActionBar().hide();
+
+
+        backButton_cat = (ImageView) findViewById(R.id.new_cat_back_icon);
+        backButton_cat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_new_cat, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
